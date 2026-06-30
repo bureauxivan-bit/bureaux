5d7caa8e9697ac0a5f756ab4e2d7dc5ea209ff81
Ivan Rudenko <bureaux.ivan@gmail.com>
2026-06-30 15:15:45 +0300

    seo(P0-5): расширить JSON-LD: @graph, areaServed, priceRange, hasOfferCatalog, WebSite

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -12,15 +12,37 @@ export default async function SiteLayout({ children }: { children: React.ReactNo
 
   const jsonLd = {
     '@context': 'https://schema.org',
-    '@type': 'LocalBusiness',
-    name: 'bureau x',
-    description: "Бюро архітектури та дизайну інтер'єрів у стилі МУАС.",
-    url: SITE_URL,
-    telephone: settings.phone,
-    email: settings.email,
-    address: { '@type': 'PostalAddress', addressLocality: 'Київ', addressCountry: 'UA' },
-    geo: { '@type': 'GeoCoordinates', latitude: 50.45444, longitude: 30.52361 },
-    sameAs: [settings.instagram, settings.facebook, settings.behance].filter(Boolean),
+    '@graph': [
+      {
+        '@type': ['LocalBusiness', 'ProfessionalService'],
+        name: 'Bureau X',
+        description: "Архітектурне бюро та студія дизайну інтер'єрів. Авторський стиль МУАС.",
+        url: SITE_URL,
+        telephone: settings.phone,
+        email: settings.email,
+        address: { '@type': 'PostalAddress', addressLocality: 'Київ', addressCountry: 'UA' },
+        geo: { '@type': 'GeoCoordinates', latitude: 50.45444, longitude: 30.52361 },
+        areaServed: ['Київ', 'Україна'],
+        priceRange: '$$$$',
+        openingHours: 'Mo-Fr 09:00-18:00',
+        sameAs: [settings.instagram, settings.facebook, settings.behance].filter(Boolean),
+        hasOfferCatalog: {
+          '@type': 'OfferCatalog',
+          itemListElement: [
+            { '@type': 'Service', name: 'Дизайн інтер\'єру', url: `${SITE_URL}/posluhy/dyzajn-intereru` },
+            { '@type': 'Service', name: 'Архітектурне проєктування', url: `${SITE_URL}/posluhy/arkhitektura` },
+            { '@type': 'Service', name: 'Ремонт під ключ', url: `${SITE_URL}/posluhy/remont-pid-klyuch` },
+            { '@type': 'Service', name: 'Комерційні приміщення', url: `${SITE_URL}/posluhy/komertsiini-prymishchennia` },
+            { '@type': 'Service', name: 'Приватні простори', url: `${SITE_URL}/posluhy/pryvatni-prostory` },
+          ],
+        },
+      },
+      {
+        '@type': 'WebSite',
+        name: 'Bureau X',
+        url: SITE_URL,
+      },
+    ],
   };
 
   return (
